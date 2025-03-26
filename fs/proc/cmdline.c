4cbfa53f26c62ece4537442df120ca2df93119e9
ardia-kun <ardiayogabekti@gmail.com>
2025-03-26 18:54:05 +0700

    KernelSU: Introduce New KernelSU-Next with SUSFS
    Signed-off-by: ardia-kun <ardiayogabekti@gmail.com>

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -3,9 +3,12 @@
 #include <linux/init.h>
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
-
 #ifdef CONFIG_INITRAMFS_IGNORE_SKIP_FLAG
 #include <asm/setup.h>
+#endif
+
+#ifdef CONFIG_KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG
+extern int susfs_spoof_cmdline_or_bootconfig(struct seq_file *m);  // Deklarasi fungsi
 #endif
 
 #ifdef CONFIG_INITRAMFS_IGNORE_SKIP_FLAG
@@ -13,56 +16,56 @@
 #define INITRAMFS_STR_REPLACE "want_initramfs"
 #define INITRAMFS_STR_LEN (sizeof(INITRAMFS_STR_FIND) - 1)
 
-#ifdef CONFIG_KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG
-extern int susfs_spoof_cmdline_or_bootconfig(struct seq_file *m);
-#endif
-
 static char proc_command_line[COMMAND_LINE_SIZE];
 
 static void proc_command_line_init(void) {
-	char *offset_addr;
+    char *offset_addr;
 
-	strcpy(proc_command_line, saved_command_line);
+    strcpy(proc_command_line, saved_command_line);
 
-	offset_addr = strstr(proc_command_line, INITRAMFS_STR_FIND);
-	if (!offset_addr)
-		return;
+    offset_addr = strstr(proc_command_line, INITRAMFS_STR_FIND);
+    if (!offset_addr)
+        return;
 
-	memcpy(offset_addr, INITRAMFS_STR_REPLACE, INITRAMFS_STR_LEN);
+    memcpy(offset_addr, INITRAMFS_STR_REPLACE, INITRAMFS_STR_LEN);
 }
 #endif
 
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
 #ifdef CONFIG_KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG
-	if (!susfs_spoof_cmdline_or_bootconfig(m)) {
-		seq_putc(m, '\n');
-		return 0;
-	}
+    if (!susfs_spoof_cmdline_or_bootconfig(m)) {
+        seq_putc(m, '\n');
+        return 0;
+    }
 #endif
-
 #ifdef CONFIG_INITRAMFS_IGNORE_SKIP_FLAG
-	seq_printf(m, "%s\n", proc_command_line);
+    seq_printf(m, "%s\n", proc_command_line);
 #else
-	seq_printf(m, "%s\n", saved_command_line);
-	return 0;
+    seq_printf(m, "%s\n", saved_command_line);
+#endif
+    return 0;
 }
 
 static int cmdline_proc_open(struct inode *inode, struct file *file)
 {
-	return single_open(file, cmdline_proc_show, NULL);
+    return single_open(file, cmdline_proc_show, NULL);
 }
 
 static const struct file_operations cmdline_proc_fops = {
-	.open		= cmdline_proc_open,
-	.read		= seq_read,
-	.llseek		= seq_lseek,
-	.release	= single_release,
+    .open       = cmdline_proc_open,
+    .read       = seq_read,
+    .llseek     = seq_lseek,
+    .release    = single_release,
 };
 
 static int __init proc_cmdline_init(void)
 {
-	proc_create("cmdline", 0, NULL, &cmdline_proc_fops);
-	return 0;
+#ifdef CONFIG_INITRAMFS_IGNORE_SKIP_FLAG
+    proc_command_line_init();
+#endif
+
+    proc_create("cmdline", 0, NULL, &cmdline_proc_fops);
+    return 0;
 }
 fs_initcall(proc_cmdline_init);
